bfdf12de5f88a519f99d6770703acbb135da789c
robitu <robertoiturralde2009@hotmail.com>
2024-03-20 14:06:05 -0600

    testing branch

--- ignition/modules/Lock.ts
+++ ignition/modules/Lock.ts
@@ -1,6 +1,6 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const JAN_1ST_2030 = 1893456000;
+const JAN_1ST_2030 = 1893456000; //helo
 const ONE_GWEI: bigint = 1_000_000_000n;
 
 const LockModule = buildModule("LockModule", (m) => {
