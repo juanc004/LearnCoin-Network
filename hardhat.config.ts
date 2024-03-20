720deecaccf5f364df099b2069f03133d09cb85d
juanc004 <124308226+juanc004@users.noreply.github.com>
2024-03-20 18:32:04 -0500

    installed dotenv package, updated hardhat config, and had successful test deployment on sepolia testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+// import "dotenv/config";
+
+const { PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: "",
+      // accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
   solidity: "0.8.24",
 };
 
